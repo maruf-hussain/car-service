e37063e26c48a9e871ab4d60a6b524f0946d1462
maruf-hussain <engmarufhussain@gmail.com>
2024-04-24 14:12:16 +0600

    jfaslj

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import Footer from "../Pages/Shared/Footer/Footer";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+          path:'/',
+          element: <Home></Home>
+        },
+        
+      ]
+    },
+  ]);
+  export default router;
