b84cb77b6580828f1de65453d2e5d4efff7b0d56
maruf-hussain <engmarufhussain@gmail.com>
2024-05-01 01:10:39 +0600

    a;jf

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,26 @@
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='lg: w-1/2 relative'>
+                    <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                    <img src={parts} className="w-1/2 rounded-lg absolute right-5 top-1/2 border-white border-8 shadow-2xl" />
+                </div>        
+                  <div className='lg: w-1/2 space-y-3 p-5'>
+                    <h3 className='text-red-500 font-bold text-4xl'>About Us</h3>
+                    <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.<p/> 
+                    
+                    <p className='py-2'> the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                    .</p>
+                    <button className="btn bg-red-400">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -7,14 +7,14 @@ const Footer = () => {
         <footer className="footer p-10 bg-black text-white">
   <aside>
     <img className='w-28' src={logo} alt="" />
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+    <p>Maruf Car Service Ltd.<br/>Providing reliable tech since 1990</p>
   </aside> 
   <nav>
     <h6 className="footer-title">Services</h6> 
     <a className="link link-hover">Branding</a>
     <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Painting</a>
+    <a className="link link-hover">Denting    </a>
   </nav> 
   <nav>
     <h6 className="footer-title">Company</h6> 
